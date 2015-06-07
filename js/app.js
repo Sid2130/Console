bd1e71aa4a8622352f69e781298d9e4a779711f1
Sadique <mdsadique2@gmail.com>
2015-06-07 14:45:07 +0530

    Caret Position

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@
     $("#jsCode").focus();
     $("#jsCode").autogrow();
    
+    app.getCodesExecutedList();
     app.createListFromLocal();
-    var autocompleteSource = app.jsTags.concat(app.codesExecuted);
+
+    var autocompleteSource = app.jsTags.concat(app.codesExectedList);
+    //console.log(JSON.stringify(autocompleteSource));
     $( "#jsCode" ).autocomplete({
       source: autocompleteSource
     });
@@ -35,7 +38,59 @@ var app = {
         'keys', 'length', 'localStorage', 'new','Object', 'onblur', 'onclick', 'ondblclick', 'prompt', 'return', 'sessionStorage', 'stop', 
         'this', 'throw', 'toString', 'typeof', 'try', 'valueOf', 'values', 'var', 'void', 'while', 'window', 'with' 
     ],
-    codesExecuted : [],
+    
+    codesExecutedObjectsArray: [],
+    codesExectedList: [],
+
+
+    addCodeToConsole: function(htmlObject){
+        $("#jsCode").val($(htmlObject).text().trim());
+        this.closeMenu();
+    },
+
+
+    addInHistoryMenu: function(codeEntered, status){
+        if(status === 'error'){
+            $('#history-menu').append('<div class="menu-elements error" onclick="app.addCodeToConsole(this);"><i class="fa fa-exclamation-triangle pull-left"></i><span class="text">'+codeEntered+'</span></div>');
+        }
+        else{
+            $('#history-menu').append('<div class="menu-elements success" onclick="app.addCodeToConsole(this);"><i class="fa fa-check-square pull-left"></i><span class="text">'+codeEntered+'</span></div>');
+        }  
+    },
+
+
+    clearHistory: function(){
+        var clearHistoryConfirm = confirm("Are you sure you want to clear history?")
+        if(clearHistoryConfirm){
+            localStorage.setItem("executedCodes","");
+            $('#history-menu .menu-elements').fadeOut(function(){
+                $(this).remove();
+            });
+            this.closeMenu();
+        }
+        else{
+            return;
+        }
+        
+    },
+
+    clearOutputScreen: function(){
+        $('.output-box').children().fadeOut(function(){
+            $(this).remove();
+        });
+    },
+
+
+    createListFromLocal: function(){
+        var storedObject = this.getCodesExecutedObject();
+        if(storedObject === '' || storedObject === null){
+            return;
+        }
+        this.codesExecutedObjectsArray = storedObject;
+        for(var index=0; index<storedObject.length; index++){
+            this.addInHistoryMenu(storedObject[index].code, storedObject[index].status);
+        }
+    },
 
     closeMenu: function(){
         $('#history-menu').removeClass('open');
@@ -43,16 +98,56 @@ var app = {
         $('.menu-overlay').fadeOut();
     },
 
+
+    executeScript: function(codeReceived){
+        var response;
+        try{
+            response = window.eval(codeReceived);
+        }
+        catch(error){
+            return {status: 'error', message: error};
+        }
+        return {status: 'ok', message: response};
+    },
+
+
+
+    
+    getCodesExecutedList: function(){
+        var storedObject = this.getCodesExecutedObject();
+        for(var index=0; index<storedObject.length; index++){
+            app.codesExectedList.push(storedObject[index].code);
+        }
+    },
+
+
+    getCodesExecutedObject: function(){
+        var storedObject = localStorage.getItem("executedCodes");
+        if(storedObject === '' || storedObject === null){
+            return '';
+        }
+        storedObject = JSON.parse(storedObject);
+        return storedObject;
+    },
+
+    getLineNumber: function(){
+        var inputText = $("#jsCode").val();
+        return inputText.substr(0, $("#jsCode").selectionStart).split("\n").length;
+    },
+
+    lastLineOfInput: function(){
+        var inputText = $("#jsCode").val();
+        return inputText.lastIndexOf("\n")+1;
+    },
+
+
     openMenu: function(){
         $('#history-menu').addClass('open');
         $('.console-box').addClass('zoom-class');
         $('.menu-overlay').fadeIn();
     },
 
-    addCodeToConsole: function(htmlObject){
-        $("#jsCode").val($(htmlObject).text().trim());
-        this.closeMenu();
-    },
+    
 
     processCode: function(){
         var executedCodesObject = {};
@@ -78,29 +173,13 @@ var app = {
         executedCodesObject.status = response.status;
         executedCodesObject.code = codeEntered;
 
-        this.codesExecuted.push(executedCodesObject);
-        localStorage.setItem("executedCodes", JSON.stringify(this.codesExecuted));
+        this.codesExecutedObjectsArray.push(executedCodesObject);
+        localStorage.setItem("executedCodes", JSON.stringify(this.codesExecutedObjectsArray));
     },
 
 
-    executeScript: function(codeReceived){
-        var response;
-        try{
-            response = window.eval(codeReceived);
-        }
-        catch(error){
-            return {status: 'error', message: error};
-        }
-        return {status: 'ok', message: response};
-    },
+   
 
-
-
-    clearOutputScreen: function(){
-        $('.output-box').children().fadeOut(function(){
-            $(this).remove();
-        });
-    },
 
     printMessage: function(message, status){
         if(status === 'error'){
@@ -115,30 +194,12 @@ var app = {
         else{
             $('.output-box').append('<div class="console-response color-blue">'+message+'</div>');
         }
-    },
-
-    addInHistoryMenu: function(codeEntered, status){
-        if(status === 'error'){
-            $('#history-menu').append('<div class="menu-elements error" onclick="app.addCodeToConsole(this);"><i class="fa fa-exclamation-triangle pull-left"></i><span class="text">'+codeEntered+'</span></div>');
-        }
-        else{
-            $('#history-menu').append('<div class="menu-elements success" onclick="app.addCodeToConsole(this);"><i class="fa fa-check-square pull-left"></i><span class="text">'+codeEntered+'</span></div>');
-        }
-        
-    },
-
-
-    createListFromLocal: function(){
-        var storedObject = localStorage.getItem("executedCodes");
-        if(storedObject === '' || storedObject === null){
-            return;
-        }
-        storedObject = JSON.parse(storedObject);
-        this.codesExecuted = storedObject;
-        for(var index=0; index<storedObject.length; index++){
-            this.addInHistoryMenu(storedObject[index].code, storedObject[index].status);
-        }
     }
+   
+    
+
+
+    
 };
 
 
